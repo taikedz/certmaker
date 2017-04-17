fb63bba940e0247c794f8844469d2e1dc180accd
Tai <#dch tai gmail>
2017-04-17 10:20:10 +0100

    Working release with all-perms CSR
    * Ability to create CA self-signed certs, and regular non self-signed certs with CSR
    * ISSUE: regular certs also request ability to sign certs
    	* this is likely a config issue in need of documentation
    * Moved workflow discussion out of script help and into README

--- README.md
+++ README.md
@@ -2,4 +2,62 @@
 
 A comand line tool for creating and managing certificate signing.
 
+WORKFLOW
+========
+
+Root CA
+-------
+
+Your organisation should only have one Root CA (Certificate Authority). Use the `crete-root-ca` command in the creation of this one root certificate.
+
+This will create threee files:
+
+1. The Key file - keep this EXTREMELY safe. It is the lynchpin of your chain of trust and must by no means ever be provided by an unauthorized user.
+	* Protect it with a password when the prompt appears during creation
+	* Consider giving the key data to one operative, and the passphrase to another.
+
+2. A CSR file - certifcate signing request.
+	* A request file that says "please derive a certificate from me using someone's key"
+	* Allows you to have you signed by an external authority, for example VeriSign, COMODO, etc
+	* In this instance, it will be used for the self-signing, but being signed by a trusted thrid party instead is also valid
+	* the combination of a CSR and a Key leads to the creation of a Certificate
+
+3. The Certificate file - this is the public certificate that other programs and services will use to ascertain the validity of your identity.
+	* This certificate will in this case also be the root certificate of your trust chain.
+	* It guarantees that anything that is signed by your (super private) key is thus signed by you.
+
+
+Device or Site Certificate
+--------------------------
+
+A device or website can be issued a certificate - it needs its own key, and its own CSR first. Use the `create` command to create a Key + CSR pair for said site or device.
+
+Send the CSR file to the authority who is in control od the Root certificate for them to sign it. They will return a Certificate file for you.
+
+Deploy the Key and the Certificate to the device or website as required.
+
+The defaults for regular certificate creation is set up so that the issued certificate cannt be used for signing in turn.
+
+Use the --allow-signing additional option enable it.
+
+### Department CA
+
+Any sub-organisation then can create their own key with which to sign certificates - they should do this with the `create` command.
+
+They needs their CSR to be signed by the organisation's Root CA ; after which, they will be able to sign certificates themselves - unless the Root CA issues the certificate without the permission to sign against.
+
+Signing a CSR
+-------------
+
+As a Certificate Authority (either as Root for the organisation, or as CA for your department), you can sign CSRs for sites and devices you have reponsibility for.
+
+You need the CSR from the requestor, as well as your own Key and Certificate files,
+
+You can then use the `sign` option on the Key, Certificate and requestor CSR to generate a sigfned certificate to return to the requestor.
+
+
+Known Issues
+============
+
+* regular certificate CSR requests ability to sign CRLs in turn
 
--- cert-maker.sh
+++ cert-maker.sh
@@ -60,7 +60,7 @@ function self_sign_certificate {
 
 	argcheck "$certname" certificate name
 
-	openssl "x509" -days "$ndays" -in "$certname.csr" -signkey "$certname.key" -out "$certname.crt" || faile $? "Failed to create self-signed certificate"
+	openssl "x509" -req -days "$ndays" -in "$certname.csr" -signkey "$certname.key" -out "$certname.crt" || faile $? "Failed to create self-signed certificate"
 }
 
 function sign_certificate {
@@ -118,82 +118,35 @@ Create a CA root certificate (should only need to be done once per department) (
 
 	docert create-root-ca ca-name [DAYS]
 
+
 Create a device/site certificate:
 
 	docert create device-or-site-name
 
-Sign a device's CSR to produce its approved certificate (specifying the number of DAYS the certificate should be valid for):
 
-	docert sign ca-name device-or-site-name [DAYS] [OPTIONS ... ]
+Sign a device's CSR to produce its approved certificate (optionally specifying the number of DAYS the certificate should be valid for):
+
+	docert sign ca-name device-or-site-name [DAYS]
+
 
 View a CSR's contents
 
 	docert viewcsr CSRFILE
 
+
 View a Certificate's contents
 
 	docert viewcert CERTFILE
+
 
 Check that a TARGETCERT was signed by a CACERT
 
 	docert verify CACERT TARGETCERT
 
-WORKFLOW
-========
-
-Root CA
--------
-
-Your organisation should only have one Root CA (Certificate Authority). Use the `crete-root-ca` command in the creation of this one root certificate.
-
-This will create threee files:
-
-1. The Key file - keep this EXTREMELY safe. It is the lynchpin of your chain of trust and must by no means ever be provided by an unauthorized user.
-	* Protect it with a password when the prompt appears during creation
-	* Consider giving the key data to one operative, and the passphrase to another.
-
-2. A CSR file - certifcate signing request.
-	* A request file that says "please derive a certificate from me using someone's key"
-	* Allows you to have you signed by an external authority, for example VeriSign, COMODO, etc
-	* In this instance, it will be used for the self-signing, but being signed by a trusted thrid party instead is also valid
-	* the combination of a CSR and a Key leads to the creation of a Certificate
-
-3. The Certificate file - this is the public certificate that other programs and services will use to ascertain the validity of your identity.
-	* This certificate will in this case also be the root certificate of your trust chain.
-	* It guarantees that anything that is signed by your (super private) key is thus signed by you.
-
-
-Device or Site Certificate
---------------------------
-
-A device or website can be issued a certificate - it needs its own key, and its own CSR first. Use the `create` command to create a Key + CSR pair for said site or device.
-
-Send the CSR file to the authority who is in control od the Root certificate for them to sign it. They will return a Certificate file for you.
-
-Deploy the Key and the Certificate to the device or website as required.
-
-The defaults for regular certificate creation is set up so that the issued certificate cannt be used for signing in turn.
-
-Use the --allow-signing additional option enable it.
-
-### Department CA
-
-Any sub-organisation then can create their own key with which to sign certificates - they should do this with the `create` command.
-
-They needs their CSR to be signed by the organisation's Root CA ; after which, they will be able to sign certificates themselves - unless the Root CA issues the certificate without the permission to sign against.
-
-Signing a CSR
--------------
-
-As a Certificate Authority (either as Root for the organisation, or as CA for your department), you can sign CSRs for sites and devices you have reponsibility for.
-
-You need the CSR from the requestor, as well as your own Key and Certificate files,
-
-You can then use the `sign` option on the Key, Certificate and requestor CSR to generate a sigfned certificate to return to the requestor.
-
 
 EOF
 exit 0
+
 fi
 
 case "$action" in
