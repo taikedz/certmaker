e2f972da3adc29ec97f937da8e3b36b12ad5c879
Tai @ Flex <dch.tai@gmail.com>
2017-04-17 20:23:50 +0100

    functional but dirty cert-getter

--- cert-getter.sh
+++ cert-getter.sh
@@ -15,12 +15,28 @@ function argcheck {
 	fi
 }
 
-funciton get_cert_info {
-	openssl x509 -text -noout -in "$tmpcert"
+function get_cert_info {
+	openssl x509 -text -noout -in "$1"
 }
 
 function get_issuer_cert {
-# example line -- CA Issuers - URI:http://cert.int-x3.letsencrypt.org/
+	local issuerline="$1"
+	if [[ -z "$issuerline" ]]; then
+		return
+	fi
+	: # example line -- CA Issuers - URI:http://cert.int-x3.letsencrypt.org/
+	local CAURI="${issuerline#*URI:}"
+	# May be able to get the CA name by grepping Issuer and finginf the CN= part
+	local caname="$(echo "$CAURI"|md5sum)"
+	local caname="ca-${caname:0:10}.crt" # Same CA, same filename
+
+	# should we check for existing cert that has expired ?
+
+	local bintemp=$(mktemp)
+	wget -q -O "$bintemp" "$CAURI"
+	openssl x509 -inform der -in "$bintemp" -out "$bintemp".pem
+
+	sudo cp "$bintemp".pem "$domaindir/$caname"
 }
 
 action="$1"; shift
@@ -43,16 +59,16 @@ openssl s_client -servername "$domain" -connect "$connectstring" </dev/null | se
 case "$action" in
 add)
 	sudo mkdir "$domaindir" -p
+	sudo chmod 755 "$domaindir"
 	sudo mv "$tmpcert" "$domaindir/$domain.crt"
+	sudo chmod 644 "$domaindir/$domain.crt"
 
-	issuer="$(get_cert_info "$domaindir/$domain.crt"|grep 'CA Issuers - URI:')"
-	if [[ -n "$issuer" ]]; then
-	fi
+	get_issuer_cert "$(get_cert_info "$domaindir/$domain.crt"|grep 'CA Issuers - URI:')"
 
 	sudo dpkg-reconfigure ca-certificates
 	;;
 view)
-	get_cert_info
+	get_cert_info "$tmpcert"
 	;;
 *)
 	faile Invalid action
